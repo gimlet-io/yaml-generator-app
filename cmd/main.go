974c112b4cb81cb2cdf25171fe71a1501368c711
Laszlo Fogas <laszlo@gimlet.io>
2023-04-19 08:18:09 +0200

    Revert "Revert "CORS net/http middleware""
    This reverts commit 96c50788782df813cbcd02ee2cbfa7f68534bb70.

--- cmd/main.go
+++ cmd/main.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -13,6 +14,17 @@ func main() {
 	fmt.Println("App init..")
 
 	r := chi.NewRouter()
+
+	// Basic CORS
+	// for more ideas, see: https://developer.github.com/v3/#cross-origin-resource-sharing
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"}, // Use this to allow specific origin hosts
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
 	r.Use(middleware.RequestID)
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
